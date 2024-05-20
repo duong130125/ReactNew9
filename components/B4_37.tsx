843555c6e838f26657abe961395dc27218d14ec7
duong130125 <nguyendinhduong2005yt@gmail.com>
2024-05-20 12:16:30 +0700

    add

--- components/B4_37.tsx
+++ components/B4_37.tsx
@@ -4,14 +4,15 @@ import { ThemeContext } from './B1_37'
 export default function B4_37() {
     const theme = useContext(ThemeContext)
     const [backgroundColor, setBackgroundColor] = useState(theme);
+    const [colorName, setColorName] = useState<string>('blue');
 
     const toggle = () => {
         setBackgroundColor(backgroundColor === 'blue' ? 'red' : 'blue');
+        setColorName(backgroundColor === 'blue' ? 'red' : 'blue');
     };
-    const colorName = 
   return (
     <>
-    <h4>{colorName}</h4>
+    <h4>Nền {colorName}</h4>
     <button  style={{backgroundColor: backgroundColor,color:"white",width:"100px",height:"40px",borderRadius:'5px'}} onClick={toggle}>
         Click me 
     </button>
